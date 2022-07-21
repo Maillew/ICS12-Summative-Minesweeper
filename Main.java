93dfffb1faa726b375fa723aec510496385fa5b3
Maillew <63217833+Maillew@users.noreply.github.com>
2022-07-20 23:14:22 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+// Main class: runs the main menu 
+// William Lin and Matthew Liu
+// Jan 21, 2022
+class Main {
+  public static void main(String[] args) {
+	  MainMenu menu = new MainMenu();
+  }
+}
